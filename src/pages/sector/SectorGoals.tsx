14800d9be11a85f69a15aff0d2120fbcafc0ef9b
Gabriel Pinto Andrade <gabriel.andrade@aguiasistemas.com.br>
2025-12-30 10:39:12 -0300

    add: busca em metas do setor

--- src/pages/sector/SectorGoals.tsx
+++ src/pages/sector/SectorGoals.tsx
@@ -1,7 +1,9 @@
 import { useParams } from "react-router-dom";
-import { Users } from "lucide-react";
+import { Search, Users } from "lucide-react";
 import { useSectorGoals } from "@/hooks/useGoals";
 import { GoalCard } from "@/components/GoalCard"; // Reutilizando!
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
 
 export default function SectorGoalsPage() {
   // Pega o ID da rota: /sector/:sectorId/goals
@@ -9,9 +11,15 @@ export default function SectorGoalsPage() {
 
   const { data: goals, isLoading } = useSectorGoals(sectorId);
 
+  const [searchTerm, setSearchTerm] = useState("");
+
   if (isLoading) {
     return <div className="p-10">Carregando metas do setor...</div>; // Ou use seu Skeleton
   }
+
+  const filteredGoals = goals?.filter((goal) =>
+    goal.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div className="flex flex-col gap-6">
@@ -25,10 +33,21 @@ export default function SectorGoalsPage() {
             Visão gerencial das metas ativas da sua equipe.
           </p>
         </div>
+
+        <div className="relative w-full md:w-75">
+          <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+          <Input
+            type="search"
+            placeholder="Buscar pelo título..."
+            className="pl-9 bg-background" // pl-9 dá espaço para o ícone
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
       </div>
 
       <div className="grid gap-6 md:grid-cols-2 xl:grid-cols-3">
-        {goals?.map((goal) => (
+        {filteredGoals?.map((goal) => (
           <GoalCard key={goal.id} goal={goal} />
         ))}
       </div>
